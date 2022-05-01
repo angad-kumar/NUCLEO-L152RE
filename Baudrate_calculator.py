2b6d6ac43746e10c979ccb5cbdc0461bdb30361f
Angad Kumar <angad9745@gmail.com>
2022-05-01 23:25:39 +0530

    Update Baudrate_calculator.py

--- Baudrate_calculator.py
+++ Baudrate_calculator.py
@@ -1,12 +1,20 @@
+
 baud_rate = 9600 # Change for desired baudrate
 clock = 32000000
 
 usart_div = clock/(16 * baud_rate)
 
 # print(usart_div)
-before_uart = hex(int(usart_div))
+before_uart = int(usart_div)
 after_uart = int((usart_div % 1) * 16) + 1
 
 # print(usart_div)
-print(before_uart)
+print(hex(before_uart))
 print(after_uart)
+
+
+
+print("BRR Register Value: ",hex( (before_uart<<4) | after_uart ))
+# print(string(before_uart))
+
+# print(bytes.fromhex(before_uart).decode('utf-8'))
